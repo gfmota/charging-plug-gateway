8f0828906dc871d46ab977a6bea1d28c795fde39
Gabriel Mota <63614154+gfmota@users.noreply.github.com>
2024-05-03 11:08:14 -0300

    Initial commit

---
+++ src/test/java/com/template/template/TemplateApplicationTests.java
@@ -0,0 +1,11 @@
+package com.template.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
