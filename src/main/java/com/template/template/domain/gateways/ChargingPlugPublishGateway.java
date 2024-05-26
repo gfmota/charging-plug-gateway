c353c02e1619a7e987c578ea229647844f259ed0
gfmota <gabrielfmota134@gmail.com>
2024-05-25 23:37:12 -0300

    add message based gateway
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

---
+++ src/main/java/com/template/template/domain/gateways/ChargingPlugPublishGateway.java
@@ -0,0 +1,10 @@
+package com.template.template.domain.gateways;
+
+import com.template.template.domain.entity.ChargingPlugStationCurrentStatus;
+import com.template.template.domain.entity.ChargingPlugStationRecord;
+
+public interface ChargingPlugPublishGateway {
+    void publishChargingPlugStationDailyReport(ChargingPlugStationRecord chargingPlugStationRecord);
+
+    void publishChargingPlugStationCurrentStatus(ChargingPlugStationCurrentStatus chargingPlugStationCurrentStatus);
+}
---
+++ src/main/java/com/template/template/domain/usecases/ChargingPlugStationPublisherUseCase.java
@@ -0,0 +1,34 @@
+package com.template.template.domain.usecases;
+
+import com.template.template.domain.entity.ChargingPlugStationCurrentStatus;
+import com.template.template.domain.entity.ChargingPlugStationRecord;
+import com.template.template.domain.gateways.ChargingPlugPublishGateway;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ChargingPlugStationPublisherUseCase {
+
+    @Autowired
+    private ChargingPlugRecordUsecase chargingPlugRecordUsecase;
+    @Autowired
+    private ChargingPlugPublishGateway chargingPlugPublishGateway;
+
+    @Scheduled(cron = "0 */5 * * * ?")
+    private void notifyLastStatusSubscribers() {
+        log.info("Publishing Charging Plug Stations last status");
+        final ChargingPlugStationCurrentStatus currentStatus = chargingPlugRecordUsecase.getChargingPlugCurrentStatus();
+        chargingPlugPublishGateway.publishChargingPlugStationCurrentStatus(currentStatus);
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    private void notifyDailySubscribers() {
+        log.info("Publishing Charging Plug Stations daily report");
+        final ChargingPlugStationRecord report = chargingPlugRecordUsecase.getChargingPlugRecordFromLastDay();
+        chargingPlugPublishGateway.publishChargingPlugStationDailyReport(report);
+    }
+
+}
