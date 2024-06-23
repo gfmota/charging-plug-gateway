8cd6fa0d8f807e9cf6aca7be897c2ebdb89222b1
gfmota <gabrielfmota134@gmail.com>
2024-06-22 22:49:47 -0300

    subscribing log
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- src/main/java/com/template/template/infrastructure/controller/ChargingPlugStationController.java
+++ src/main/java/com/template/template/infrastructure/controller/ChargingPlugStationController.java
@@ -2,6 +2,7 @@ package com.template.template.infrastructure.controller;
 
 import com.template.template.domain.entity.SubscribedService;
 import com.template.template.domain.usecases.SubscriberUseCase;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
+@Slf4j
 public class ChargingPlugStationController {
     @Autowired
     private SubscriberUseCase subscriberUseCase;
@@ -16,6 +18,7 @@ public class ChargingPlugStationController {
     @PostMapping("/subscribe")
     private ResponseEntity<?> subscribe(final @RequestBody SubscribedService subscribedService) {
         try {
+            log.info("Subscribing to service: {}", subscribedService);
             subscriberUseCase.subscribe(subscribedService);
             return ResponseEntity.ok().build();
         } catch (final Exception e) {
