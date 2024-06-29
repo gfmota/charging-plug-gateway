4e90723a5b2aa519b452debe89af9e3acf6e86df
gfmota <gabrielfmota134@gmail.com>
2024-06-29 19:07:25 -0300

    feat: add rabbitmq startup configuration
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- src/main/java/com/template/template/domain/usecases/ChargingPlugStationPublisherUseCase.java
+++ src/main/java/com/template/template/domain/usecases/ChargingPlugStationPublisherUseCase.java
@@ -17,14 +17,16 @@ public class ChargingPlugStationPublisherUseCase {
     @Autowired
     private ChargingPlugPublishGateway chargingPlugPublishGateway;
 
-    @Scheduled(cron = "0 */5 * * * ?")
+    @Scheduled(cron = "0 * * * * ?")
+//    @Scheduled(cron = "0 */5 * * * ?")
     private void notifyLastStatusSubscribers() {
         log.info("Publishing Charging Plug Stations last status");
         final ChargingPlugStationCurrentStatus currentStatus = chargingPlugRecordUsecase.getChargingPlugCurrentStatus();
         chargingPlugPublishGateway.publishChargingPlugStationCurrentStatus(currentStatus);
     }
 
-    @Scheduled(cron = "0 0 0 * * ?")
+    @Scheduled(cron = "30 * * * * ?")
+//    @Scheduled(cron = "0 0 0 * * ?")
     private void notifyDailySubscribers() {
         log.info("Publishing Charging Plug Stations daily report");
         final ChargingPlugStationRecord report = chargingPlugRecordUsecase.getChargingPlugRecordFromLastDay();
