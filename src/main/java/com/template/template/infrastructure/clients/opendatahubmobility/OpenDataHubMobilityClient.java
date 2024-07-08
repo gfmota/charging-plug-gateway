241581cdf53b7baf49a1ec8ec46e57f8779f0f9b
gfmota <gabrielfmota134@gmail.com>
2024-07-07 22:14:59 -0300

    add cache
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- src/main/java/com/template/template/infrastructure/clients/opendatahubmobility/OpenDataHubMobilityClient.java
+++ src/main/java/com/template/template/infrastructure/clients/opendatahubmobility/OpenDataHubMobilityClient.java
@@ -2,6 +2,7 @@ package com.template.template.infrastructure.clients.opendatahubmobility;
 
 import com.template.template.infrastructure.clients.opendatahubmobility.dto.OpenDataHubMobilityResponseDTO;
 import lombok.NonNull;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface OpenDataHubMobilityClient {
     @GetMapping(value = "/v2/tree%2Cnode/EChargingPlug/%2A/{from}/{to}?limit=200&offset=0&where=sorigin.eq.%22DRIWE%22&shownull=false&distinct=true&timezone=UTC",
             consumes = "application/json")
+    @Cacheable("openDataHubMobilityReportFromTimeRange")
     OpenDataHubMobilityResponseDTO getReportFromTimeRange(
             @NonNull @PathVariable("from") String from, @NonNull @PathVariable("to") String to);
 
     @GetMapping(value = "/v2/tree%2Cnode/EChargingPlug/%2A/latest?limit=200&offset=0&where=sorigin.eq.%22DRIWE%22&shownull=false&distinct=true&timezone=UTC",
             consumes = "application/json")
+    @Cacheable("openDataHubMobilityLatestReport")
     OpenDataHubMobilityResponseDTO getLatestReport();
 }
